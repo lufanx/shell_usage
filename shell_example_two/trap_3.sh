ca487e12fbb7dca6a45e23b45d14461de47d8fd6
lufanx <554001112@qq.com>
2018-02-26 14:25:53 +0800

    add trap_2.sh and trap_3.sh, about jgnore and catch signal.
    Signed-off-by: lufanx <554001112@qq.com>

---
+++ shell_example_two/trap_3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo "This is test about trap command!"
+
+#catch EXIT signal
+trap "echo 'byebye'" EXIT
+
+count=1
+
+while [ $count -le 5 ]; do
+	sleep 1
+	count=$[ $count + 1 ]
+done
+
+trap - EXIT
+echo "just remove this EXIT signal"
